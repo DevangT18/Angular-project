35281b2f8e8a56406f9c1030c99725682a89a7aa
DevangT18 <129076870+DevangT18@users.noreply.github.com>
2024-05-01 13:26:30 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  baseURL: "https://nodejs-project-ab26.onrender.com/api"
 };
 
 /*
